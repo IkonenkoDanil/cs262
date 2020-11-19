dff633c81152ac5af5fb2c9e2766c26f346ac319
IkonenkoDanil <dan-ikonenko@mail.ru>
2020-11-19 23:32:48 +0300

    Add files via upload

---
+++ homework2/classes.rb
@@ -0,0 +1,48 @@
+class CustomFile < File
+
+    def initialize; end
+
+    def self.my_absolute_path(file_name)
+        # Метод, превращающий путь к файлу в его абсолютный путь (собственная реализация)
+
+        parent_count = 0
+        while file_name.start_with?("../")
+            file_name.delete_prefix!("../")
+            parent_count += 1
+        end
+
+        base_path = __dir__
+        for i in 1..parent_count
+            slash_index = base_path.rindex("/")
+            if slash_index == nil
+                break
+            end
+            base_path = base_path[0..slash_index-1]
+        end
+
+        return base_path + "/" + file_name
+    end
+
+    def self.absolute_path(file_name)
+        # Метод, превращающий путь к файлу в его абсолютный путь
+        super
+    end
+    
+
+end
+
+puts "\nRelative path: foo.txt"
+puts CustomFile.my_absolute_path("foo.txt")
+puts CustomFile.absolute_path("foo.txt")
+
+puts "\nRelative path: ../bar.txt"
+puts CustomFile.my_absolute_path("../bar.txt")
+puts CustomFile.absolute_path("../bar.txt")
+
+puts "\nRelative path: ../../baz.txt"
+puts CustomFile.my_absolute_path("../../baz.txt")
+puts CustomFile.absolute_path("../../baz.txt")
+
+puts "\nRelative path: ../../../../"
+puts CustomFile.my_absolute_path("../../../../")
+puts CustomFile.absolute_path("../../../../")
